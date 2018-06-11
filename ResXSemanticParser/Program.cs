f1ce28bfb8a5c970b1d19505382a2c9395b4bb13
Ralf Koban <rkn@mm-software.com>
2018-06-11 10:29:15 +0000

    debugging aids

--- ResXSemanticParser/Program.cs
+++ ResXSemanticParser/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ResXSemanticParser
 {
-    class Program
+    public class Program
     {
-        static int Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
             if (args.Length != 2)
                 return -1;
@@ -13,25 +15,32 @@ namespace ResXSemanticParser
             var shell = args[0]; // reserved for future usage
             var flagFile = args[1];
 
-            File.Create(flagFile).Close();
+            File.WriteAllBytes(flagFile, new byte[] { 0x42 });
 
             while (true)
             {
-                var fileToParse = Console.ReadLine();
+                var fileToParse = await Console.In.ReadLineAsync();
 
-                if ("end".Equals(fileToParse, StringComparison.OrdinalIgnoreCase))
+                if ("end".Equals(fileToParse, StringComparison.OrdinalIgnoreCase) || fileToParse == null)
                 {
                     // session is done
                     return 0;
                 }
 
-                var encodingToUse = Console.ReadLine();
-                var outputFileToWrite = Console.ReadLine();
+                var encodingToUse = Console.In.ReadLine();
+                var outputFileToWrite = Console.In.ReadLine();
 
-                var success = Parser.TryParse(fileToParse, out var yamlContent);
+                Debug.WriteLine($"File to parse: '{fileToParse}'", "RKN Semantic");
+                Debug.WriteLine($"Encoding: '{encodingToUse}'", "RKN Semantic");
+                Debug.WriteLine($"File to write: {outputFileToWrite}", "RKN Semantic");
+
+                var success = Parser.TryParse(fileToParse, out var yamlContent) ? "OK" : "KO";
+
+                Debug.WriteLine($"Parsed result: {success}", "RKN Semantic");
+
                 File.WriteAllText(outputFileToWrite, yamlContent);
 
-                Console.WriteLine(success ? "OK" : "KO");
+                Console.WriteLine(success);
             }
         }
 
@@ -41,7 +50,6 @@ namespace ResXSemanticParser
             var flagFile = args[1];
 
             Console.WriteLine($"Shell: {shell} FlagFile: {flagFile}");
-
 
             Parser.TryParse(@"D:\Private\MiKo Solutions\resx-semantic-external-parser\test.resx", out var yamlContent);
             Console.WriteLine(yamlContent);
